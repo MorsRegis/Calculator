f86fd9fe483ff60c3b9cfe6a2beada80702ee546
MorsRegis <afkouhi@gmail.com>
2016-04-15 00:49:10 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,6 @@
-# Calculator
-Basic Python implementation of a calculator fit with GUI.
-
-
 '''
-Created on Apr 14, 2016
-@author: MorsRegis
+Created on April 14, 2016
+@author: Anthony Frazier 
 '''
 
 import tkinter;
@@ -27,6 +23,7 @@ def clear():
 
 #Work on this function
 def addNumber(number):
+    newentry = ""
     newentry = entry.get() + number
     entry.set(newentry)
     return
@@ -34,7 +31,7 @@ def addNumber(number):
 def equal():
     result = ""
     result = eval(entry.get())
-    entry = entry.set(result)
+    entry.set(result)
     return 
 
 
@@ -42,63 +39,63 @@ def equal():
 frame1 = tkinter.Frame(gui)
 frame1.pack()
 button1 = tkinter.Button(frame1, padx = 20, pady = 20, text = "1", fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("1"))
+                         command = lambda : addNumber("1"))
 button1.pack(side = "left")
 button2 = tkinter.Button(frame1, text = "2", padx = 20, pady = 20,fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("2"))
+                         command = lambda : addNumber("2"))
 button2.pack(side = "left")
 button3 = tkinter.Button(frame1, text = "3", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, 
-                        command = addNumber("3"))
+                        command = lambda : addNumber("3"))
 button3.pack(side = "left")
-buttonplus = tkinter.Button(frame1, text = "+", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10)
+buttonplus = tkinter.Button(frame1, text = "+", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, command = lambda : addNumber("+"))
 buttonplus.pack(side = "left")
 
 frame2 = tkinter.Frame(gui)
 frame2.pack()
 button4 = tkinter.Button(frame2, text = "4", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("4"))
+                         command = lambda : addNumber("4"))
 button4.pack(side = "left")
 button5 = tkinter.Button(frame2, text = "5", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("5"))
+                         command = lambda : addNumber("5"))
 button5.pack(side = "left")
 button6 = tkinter.Button(frame2, text = "6", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("6"))
+                         command = lambda : addNumber("6"))
 button6.pack(side = "left")
-buttonminus = tkinter.Button(frame2, text = "-", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10)
+buttonminus = tkinter.Button(frame2, text = "-", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, command = lambda : addNumber("-"))
 buttonminus.pack(side = "left")
 
 
 frame3 = tkinter.Frame(gui)
 frame3.pack()
 button7 = tkinter.Button(frame3, text = "7", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("7"))
+                         command = lambda : addNumber("7"))
 button7.pack(side = "left")
 button8 = tkinter.Button(frame3, text = "8", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("8"))
+                         command = lambda : addNumber("8"))
 button8.pack(side = "left")
 button9 = tkinter.Button(frame3, text = "9", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("9"))
+                         command = lambda : addNumber("9"))
 button9.pack(side = "left")
-buttonstar = tkinter.Button(frame3, text = "*", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10)
+buttonstar = tkinter.Button(frame3, text = "*", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, command = lambda : addNumber("*"))
 buttonstar.pack(side = "left")
 
 frame4 = tkinter.Frame(gui, background = "Grey")
 frame4.pack()
 buttondot = tkinter.Button(frame4, text = ".", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("."))
+                         command = lambda : addNumber("."))
 buttondot.pack(side = "left")
 button0 = tkinter.Button(frame4, text = "0", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
-                         command = addNumber("0"))
+                         command = lambda : addNumber("0"))
 button0.pack(side = "left")
 button0 = tkinter.Button(frame4, text = "C", padx = 20, pady = 20,  fg ="Black", bg = "Grey", bd= 10, 
                          command = clear)
 button0.pack(side = "left")
-buttondivide = tkinter.Button(frame4, text = "/", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10)
+buttondivide = tkinter.Button(frame4, text = "/", padx = 20, pady = 20, fg ="Black", bg = "Grey", bd= 10, command = lambda : addNumber("/"))
 buttondivide.pack(side = "left")
 
 frame5 = tkinter.Frame(gui, background = "Grey")
 frame5.pack()
-buttonequals = tkinter.Button(frame5, text = "=", padx = 80, pady = 20, fg ="Black", bg = "Grey", bd= 10)
+buttonequals = tkinter.Button(frame5, text = "=", padx = 80, pady = 20, fg ="Black", bg = "Grey", bd= 10, command = lambda : equal())
 buttonequals.pack(side = "left")
 
 
